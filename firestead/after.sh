0b3cef8eecc6cb0c014e91e41834bdc92994535e
bjoern.lange <bjoern.lange@pixolith.de>
2015-08-18 13:37:32 +0200

    + added firestead and the resource setup

---
+++ firestead/after.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cd /vagrant/;
+
+# init resource dependencies, globally for vagrant/windows problems.
+npm install --global bower gulp@^3.8.8 laravel-elixir@^2.0.0;
+bower install --allow-root;
