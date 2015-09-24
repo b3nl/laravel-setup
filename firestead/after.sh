b9bc3e9cd04c8cdbb1ce5aac22142ee667baa995
bjoern.lange <bjoern.lange@pixolith.de>
2015-09-24 20:28:19 +0200

    + quit option for the bower install

--- firestead/after.sh
+++ firestead/after.sh
@@ -4,4 +4,4 @@ cd /vagrant/;
 
 # init resource dependencies, globally for vagrant/windows problems.
 npm install --global bower gulp@^3.8.8 laravel-elixir@^2.0.0;
-bower install --allow-root;
+bower install --allow-root --quiet;
